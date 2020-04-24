850991d09b72ebcf7bbf6de6fb663adfff6b3a5d
EC2 Default User <ec2-user@ip-172-31-37-163.eu-central-1.compute.internal>
2020-04-24 14:26:54 +0000

    intro text corrections

--- src/components/Scenes/Intro.js
+++ src/components/Scenes/Intro.js
@@ -32,7 +32,7 @@ export default function Intro() {
          <h1>Welcome to my text-based Adventure Game!</h1>
          <p>Most of the time the game will provide you the key words of the available choices but there will be situations when you have to figure out the answeres on your own so be careful when you type!</p>
          <p>Keep in mind that your chosen answeres may have further impact on the story!</p>
-         <p>If you die, your game is over and you have to start everything over!</p>
+         <p>You will only be able to save your game progress at the start of the Scenes but you will be able to load it anytime you want.</p>
          <ButtonContainer>
             <Button onClick={handleChange} variant="contained" color="primary">Start</Button>
          </ButtonContainer>
